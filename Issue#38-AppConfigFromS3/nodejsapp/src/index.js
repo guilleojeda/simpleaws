79855044795f6ee679512ac88cc46786b7e8d2d8
Guillermo Ojeda <>
2023-07-27 19:41:40 -0300

    Fixed errors

--- Issue#38-AppConfigFromS3/nodejsapp/src/index.js
+++ Issue#38-AppConfigFromS3/nodejsapp/src/index.js
@@ -5,19 +5,48 @@ require('./setup.js');
 const app = express();
 app.use(cors());
 const fs = require('fs');
-const fileName = process.env.FILE_NAME;
-const localFilePath = './' + fileName;
+const { AppConfigClient, GetConfigurationCommand } = require('@aws-sdk/client-appconfig');
+
+const appConfigClient = new AppConfigClient({ region: process.env.AWS_REGION });
+
+const application = process.env.APPCONFIG_APPLICATION;
+const environment = process.env.APPCONFIG_ENVIRONMENT;
+const configuration = process.env.APPCONFIG_CONFIGURATION;
+const clientId = process.env.APPCONFIG_CLIENTID;
+
+const fileNameFallback = process.env.FILE_NAME;
+const localFilePath = './' + fileNameFallback;
+
+async function fetchAppConfig() {
+  const params = {
+    Application: application,
+    Environment: environment,
+    Configuration: configuration,
+    ClientId: clientId,
+  };
+
+  try {
+    const data = await appConfigClient.send(new GetConfigurationCommand(params));
+    return data.Content.toString();
+  } catch (error) {
+    console.error(`Failed to fetch AppConfig: ${error}`);
+    return null;
+  }
+}
 
 app.get('/', async (req, res) => {
-  try {
-    const fileContents = fs.readFileSync(localFilePath, 'utf-8');
-    console.log(fileContents);
+  let message = await fetchAppConfig();
+  if (!message) {
+    try {
+      message = fs.readFileSync(localFilePath, 'utf-8');
+    } catch (e) {
+      console.log('Error: ', e);
+      res.status(500).json({ error: 'An error occurred while reading the file' });
+      return;
+    }
+  }
   res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
-  res.json({ message: fileContents });
-} catch (e) {
-  console.log('Error: ', e);
-  res.status(500).json({ error: 'An error occurred while reading the file' });
-}
+  res.json({ message: message });
 });
 
 const PORT = process.env.PORT || 3000;
--- Issue#38-AppConfigFromS3/nodejsapp/src/setup.js
+++ Issue#38-AppConfigFromS3/nodejsapp/src/setup.js
@@ -1,20 +1,44 @@
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { createWriteStream, createReadStream } = require('fs');
+const fse = require('fs-extra');
+const { pipeline, Transform } = require('stream/promises');
+const archiver = require('archiver');
+
 const bucketName = process.env.S3_BUCKET_NAME;
 const fileName = process.env.FILE_NAME;
 const region = process.env.AWS_REGION;
 const localFilePath = './' + fileName;
+const zipFileName = fileName + '.zip';
+const zipFilePath = './' + zipFileName;
 
 const s3Client = new S3Client({ region });
-const { createWriteStream } = require('fs');
-const { pipeline } = require('stream/promises');
 
-async function uploadToS3() {
+async function createZipAndUploadToS3() {
+  // Write the content to a local file
   const content = "to do: write a review for Simple AWS";
+  await fse.writeFile(localFilePath, content);
+
+  // Zip the file
+  const output = createWriteStream(zipFilePath);
+  const archive = archiver('zip', { zlib: { level: 9 } }); // Sets the compression level.
+  archive.pipe(output);
+  archive.file(localFilePath, { name: fileName });
+
+  await new Promise((resolve, reject) => {
+    archive.on('error', reject); // Rejects on error
+    output.on('close', resolve); // Resolves when the stream has been closed (and file is written).
+    output.on('error', reject); // Rejects on error
+    archive.finalize(); // Finalize the archiving process
+  }).catch(error => {
+    throw new Error(`Error occurred: ${error.message}`);
+  });
+
+  // Upload the zipped file to S3
   const params = {
     Bucket: bucketName,
-    Key: fileName,
-    Body: content,
-    ContentType: "text/plain"
+    Key: fileName + '.zip',
+    Body: createReadStream(zipFilePath),
+    ContentType: "application/zip"
   };
 
   try {
@@ -25,22 +49,7 @@ async function uploadToS3() {
   }
 }
 
-const downloadFile = async () => {
-  const downloadParams = {
-    Bucket: bucketName,
-    Key: fileName,
-  };
-  try {
-    const { Body } = await s3Client.send(new GetObjectCommand(downloadParams));
-    await pipeline(
-      Body,
-      createWriteStream(localFilePath)
-    );
-    console.log('Downloaded the file.');
-  } catch (error) {
-    console.error(`Error: ${error}`);
-  }
-};
-
-uploadToS3();
-downloadFile();
+// Ensure the upload completes before starting the download
+createZipAndUploadToS3()
+  .then(() => console.log('Upload completed'))
+  .catch(e => console.error('Error occurred:', e));
